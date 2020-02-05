5b1e02ff7841482291c27894657f3c86b4e4c2fe
Daniel Ball <g.thirteen333@gmail.com>
2020-02-05 08:02:11 -0500

    principal and annual interest rate input fields are working

--- src/com/codewithdan/Main.java
+++ src/com/codewithdan/Main.java
@@ -1,8 +1,25 @@
 package com.codewithdan;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        byte period;
+        short mortgage;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principle: " );
+        int principle = Integer.parseInt(scanner.nextLine().trim());
+
+        System.out.print("Annual Interest Rate: " );
+        float annual_interest_rate = Float.parseFloat(scanner.nextLine().trim());
+
+        System.out.print("Period: " );
+        float annual_interest_rate = Float.parseFloat(scanner.nextLine().trim());
+
     }
 }
